5856e91dc231dc49182007294b27659f682f5ccd
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2019-03-24 22:07:23 +0100

    switch hadoop-services.sh to hadoop-3.1.2

--- scripts/hadoop-services.sh
+++ scripts/hadoop-services.sh
@@ -19,11 +19,11 @@ echo "HADOOP TERASORT BENCHMARK TEST"
 TOTAL_SIZE=1G
 TOTAL_BYTES=$(numfmt --from=auto $TOTAL_SIZE)
 NUMBER_OF_100_BYTE_ROWS=$((TOTAL_BYTES / 100))
-hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.[0-9].jar teragen $NUMBER_OF_100_BYTE_ROWS /user/$USER/benchmarks/terasort-input
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.1.2.jar teragen $NUMBER_OF_100_BYTE_ROWS /user/$USER/benchmarks/terasort-input
 TERASORT_START=`date +%s` # %s means seconds since 1970-01-01 00:00:00 UTC
-hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.[0-9].jar terasort /user/$USER/benchmarks/terasort-input /user/$USER/benchmarks/terasort-output
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.1.2.jar terasort /user/$USER/benchmarks/terasort-input /user/$USER/benchmarks/terasort-output
 TERASORT_END=`date +%s` # %s means seconds since 1970-01-01 00:00:00 UTC
-hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.[0-9].jar teravalidate /user/$USER/benchmarks/terasort-output /user/$USER/benchmarks/terasort-validate
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.1.2.jar teravalidate /user/$USER/benchmarks/terasort-output /user/$USER/benchmarks/terasort-validate
 echo "TERASORT RUNTIME: $((TERASORT_END-TERASORT_START)) Seconds for sorting $TOTAL_SIZE of data."
 
 #echo "HADOOP NNBENCH TEST"
